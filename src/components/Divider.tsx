b357a6fb3c524fed1b4e8855a5cf0b664714e409
SkyG0D <silvanosilvino@hotmail.com>
2022-06-22 23:08:59 -0300

    feat: add Divider component

---
+++ src/components/Divider.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react';
+
+export function Divider() {
+  return (
+    <Box
+      w={90}
+      h={0}
+      border="1px"
+      borderColor="gray.500"
+      mx="auto"
+      mt="20"
+      mb="52px"
+    />
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Box } from '@chakra-ui/react';
 import { Banner } from '$components/Banner';
 import { Header } from '$components/Header';
 import { TravelTypes } from '$components/TravelTypes';
+import { Divider } from '$components/Divider';
 
 export default function Home() {
   return (
@@ -14,6 +15,8 @@ export default function Home() {
       <Box maxW={1200} w="100%" px="4" mx="auto" mt="20">
         <TravelTypes />
       </Box>
+
+      <Divider />
     </>
   );
 }
