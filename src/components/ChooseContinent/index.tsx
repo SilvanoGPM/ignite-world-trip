b7649c18456e525317adf12af93f3754ce6174d4
SkyG0D <silvanosilvino@hotmail.com>
2022-06-27 22:06:32 -0300

    feat: responsive ChooseContinent

--- src/components/ChooseContinent/index.tsx
+++ src/components/ChooseContinent/index.tsx
@@ -5,7 +5,13 @@ import { ContinentsSlider } from './ContinentsSlider';
 export function ChooseContinent() {
   return (
     <Box maxW={1200} w="100%" px="4" mx="auto" mb="6">
-      <Heading as="h2" textAlign="center" fontWeight={500} mb="52px">
+      <Heading
+        as="h2"
+        textAlign="center"
+        fontWeight={500}
+        mb="52px"
+        fontSize={['lg', '3xl']}
+      >
         Vamos nessa? <br /> Então escolha seu continente
       </Heading>
 
