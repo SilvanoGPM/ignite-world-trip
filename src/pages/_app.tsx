0a68e6f0372ff8afe8279cb1168183b5959e0d20
SkyG0D <silvanosilvino@hotmail.com>
2022-07-03 03:50:14 -0300

    feat: setup miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { theme } from '$/styles/theme';
+import { makeServer } from '$services/mirage';
+
+makeServer();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
