25ac98b60d03e8ecd9a17dd8a50e7318b14492a0
SkyG0D <silvanosilvino@hotmail.com>
2022-07-03 02:56:04 -0300

    refactor: responsive ContinentCities

--- src/components/Continent/ContinentCities.tsx
+++ src/components/Continent/ContinentCities.tsx
@@ -5,11 +5,20 @@ import { ContinentCityCard } from './ContinentCityCard';
 export function ContinentCities() {
   return (
     <Box mt="20">
-      <Heading as="h3" fontWeight={500} mb="10">
+      <Heading
+        as="h3"
+        fontWeight={500}
+        mb="10"
+        fontSize={['2xl', '2xl', '3xl', '4xl']}
+      >
         Cidades +100
       </Heading>
 
-      <Flex wrap="wrap" gap="2.3rem">
+      <Flex
+        wrap="wrap"
+        gap="2.3rem"
+        justifyContent={['center', 'center', 'start']}
+      >
         <ContinentCityCard
           image="/images/england.png"
           city="Londres"
